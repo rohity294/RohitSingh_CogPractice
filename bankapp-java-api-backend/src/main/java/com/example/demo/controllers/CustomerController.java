1468633ba699c9bb6343516338bacd7cf50d4a3d
Rohit Kumar Singh <rohity294@gmail.com>
2026-06-03 14:34:30 -0400

    enabled cors

--- bankapp-java-api-backend/src/main/java/com/example/demo/controllers/CustomerController.java
+++ bankapp-java-api-backend/src/main/java/com/example/demo/controllers/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.example.demo.models.Customer;
 import com.example.demo.services.CustomerService;
 
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
